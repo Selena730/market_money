54174cb8cd24b017dd3fcb60f47bb5871bdfc0e5
Sam Puttman <daputter2@gmail.com>
2024-03-25 18:40:19 -0400

    Add create to vendors routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v0 do
-      resources :vendors, only: [:show]
+      resources :vendors, only: [:show, :create]
     end
   end
 end
