7760809d50454bf90abdc9469db6d5f989d3b0de
Selena Hawamdeh <selenahawamdeh@gmail.com>
2024-03-27 16:03:20 -0600

    Test: clean test

--- spec/requests/api/v0/markets_spec.rb
+++ spec/requests/api/v0/markets_spec.rb
@@ -31,38 +31,38 @@ RSpec.describe "Api::V0::Markets", type: :request do
   end
 
   describe "GET /api/v0/markets/:id" do
-        it "returns the market and a status of 200" do #happy path test
-            market = create(:market)
-            create_list(:market_vendor, 2, market: market)
-            get "/api/v0/markets/#{market.id}"
+    it "returns the market and a status of 200" do #happy path test
+      market = create(:market)
+      create_list(:market_vendor, 2, market: market)
+      get "/api/v0/markets/#{market.id}"
 
-            expect(response).to have_http_status(200)
-            json_response = JSON.parse(response.body)
-            expect(json_response['data']['attributes']).to include(
-            'name' => market.name,
-            'street' => market.street,
-            'city' => market.city,
-            'county' => market.county,
-            'state' => market.state,
-            'zip' => market.zip,
-            'lat' => market.lat.to_s,
-            'lon' => market.lon.to_s,
-            'vendor_count' => 2
-            )
-        end
-
-        it "returns a 404 status and an error message" do # sad path test
-            invalid_id = 123123123123
-            get "/api/v0/markets/#{invalid_id}"
-    
-            expect(response).to_not be_successful
-            expect(response.status).to eq(404)
-
-            data = JSON.parse(response.body, symbolize_names: true)
-
-            expect(data[:errors]).to be_a(Array)
-            expect(data[:errors].first[:detail]).to eq("Couldn't find Market with 'id'=#{invalid_id}")
-        end
+      expect(response).to have_http_status(200)
+      json_response = JSON.parse(response.body)
+      expect(json_response['data']['attributes']).to include(
+      'name' => market.name,
+      'street' => market.street,
+      'city' => market.city,
+      'county' => market.county,
+      'state' => market.state,
+      'zip' => market.zip,
+      'lat' => market.lat.to_s,
+      'lon' => market.lon.to_s,
+      'vendor_count' => 2
+      )
     end
+
+    it "returns a 404 status and an error message" do # sad path test
+      invalid_id = 123123123123
+      get "/api/v0/markets/#{invalid_id}"
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data[:errors]).to be_a(Array)
+      expect(data[:errors].first[:detail]).to eq("Couldn't find Market with 'id'=#{invalid_id}")
+    end
+  end
 
 end
