938961babc5656dc0f403d841d162970a2a34f7d
Selena Hawamdeh <selenahawamdeh@gmail.com>
2024-03-27 16:02:36 -0600

    Feat: update routes to match the request

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,9 @@ Rails.application.routes.draw do
     namespace :v0 do
 
       resources :vendors, only: [:show, :create, :destroy]
-      resources :markets, only: [:index, :show]
-
+      resources :markets, only: [:index, :show] do
+        resources :vendors, only: [:index]
+      end
     end
   end
 end
