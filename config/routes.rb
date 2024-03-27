edff7e4d845f0283cd20c1c909b40ef1229202cc
Selena Hawamdeh <selenahawamdeh@gmail.com>
2024-03-26 22:59:44 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
     namespace :v0 do
 
       resources :vendors, only: [:show, :create]
-      resources :markets, only: [:index]
+      resources :markets, only: [:index, :show]
 
     end
   end
