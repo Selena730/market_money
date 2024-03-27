8790299a78360011981b409dadbb5c01de2fc7a4
Sam Puttman <daputter2@gmail.com>
2024-03-26 20:04:01 -0400

    Add vendors update route

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
 
-      resources :vendors, only: [:show, :create, :destroy]
+      resources :vendors, only: [:show, :create, :update, :destroy]
       resources :markets, only: [:index]
 
     end
