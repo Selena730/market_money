1df1477b77bdff47c87a5121da15006dba1cd650
Sam Puttman <daputter2@gmail.com>
2024-03-28 18:18:50 -0400

    Write out some pseudocode for nearest_atms

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -1,3 +1,5 @@
+requiire 'faraday'
+
 class Api::V0::MarketsController < ApplicationController
     def index
         markets = Market.all
@@ -7,10 +9,24 @@ class Api::V0::MarketsController < ApplicationController
     def show
         market = Market.find_by(id: params[:id])
 
-        if market 
+        if market
             render json: MarketSerializer.new(market), status: :ok
         else
             render json: {errors: [{ detail: "Couldn't find Market with 'id'=#{params[:id]}" }] }, status: :not_found
         end
+    end
+
+    def nearest_atms
+        #find the market
+
+        #if the market is not found, return a 404
+
+        #this will be in a service:
+        #make a request to tomtom
+        #https://developer.tomtom.com/search-api/documentation/search-service/category-search
+
+        #parse the response
+        #if successful - render the data with a status of ok
+        #else - render an unprocessable_entity error
     end
 end
