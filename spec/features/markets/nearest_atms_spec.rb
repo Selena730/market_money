92a616acc50a4abd863393365de41dfe8fa59bcc
Sam Puttman <daputter2@gmail.com>
2024-03-28 19:47:01 -0400

    TEST - Refactor tests to be more robust.

--- spec/features/markets/nearest_atms_spec.rb
+++ spec/features/markets/nearest_atms_spec.rb
@@ -1,22 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe Api::V0::MarketsController, type: :controller do
+RSpec.describe "Api::V0::MarketsController", type: :request do
   describe 'GET #nearest_atms', :vcr do
-    let!(:market) { create(:market) }
+    let!(:market) { create(:market, lat: "38.6270", lon: "90.1994") }
 
-    context 'when market exists' do
-      before do
-        get :nearest_atms, params: { id: market.id }
-      end
+    it 'returns a list of nearest ATMs' do
+      get "/api/v0/markets/#{market.id}/nearest_atms"
 
-      it 'returns http success' do
-        expect(response).to have_http_status(:ok)
-      end
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)['data']).not_to be_empty
+    end
 
-      it 'returns ATM data' do
-        json_response = JSON.parse(response.body)
-        expect(json_response['data']).not_to be_empty
-      end
+    it 'returns a 404 when the market is not found' do
+      get "/api/v0/markets/4654657/nearest_atms"
+
+      expect(response).to have_http_status(:not_found)
+      expect(JSON.parse(response.body)['errors']).to include('Market not found')
     end
   end
 end
